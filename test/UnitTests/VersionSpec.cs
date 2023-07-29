c0d624be9dc6a27f50a0bd394747c807cd4f56fa
Yevhen Cherkes <ycherkes@outlook.com>
2023-07-29 13:56:23 +0200

    Feature/version (#13)
    * Add Version support

---
+++ test/UnitTests/VersionSpec.cs
@@ -0,0 +1,38 @@
+using System;
+using VarDump;
+using Xunit;
+
+namespace UnitTests
+{
+    public class VersionSpec
+    {
+        [Fact]
+        public void DumpVersionSpecCsharp()
+        {
+            var version = new Version("1.2.3.4");
+
+            var dumper = new CSharpDumper();
+
+            var result = dumper.Dump(version);
+
+            Assert.Equal(
+@"var version = new Version(""1.2.3.4"");
+", result);
+        }
+
+
+        [Fact]
+        public void DumpVersionSpecVb()
+        {
+            var version = new Version("1.2.3.4");
+
+            var dumper = new VisualBasicDumper();
+
+            var result = dumper.Dump(version);
+
+            Assert.Equal(
+@"Dim versionValue = New Version(""1.2.3.4"")
+", result);
+        }
+    }
+}

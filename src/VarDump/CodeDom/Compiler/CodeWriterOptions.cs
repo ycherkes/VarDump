281860db776b1f3f6ca4af85bf244b39d7d6c4bb
Yevhen Cherkes <ycherkes@outlook.com>
2024-03-07 19:20:07 +0100

    remove unnecessary code

--- src/VarDump/CodeDom/Compiler/CodeWriterOptions.cs
+++ src/VarDump/CodeDom/Compiler/CodeWriterOptions.cs
@@ -9,25 +9,7 @@ namespace VarDump.CodeDom.Compiler;
 
 public class CodeWriterOptions
 {
-    private readonly IDictionary _options = new ListDictionary();
+    public string IndentString { get; set; }
 
-    public string IndentString
-    {
-        get
-        {
-                var o = _options[nameof(IndentString)];
-                return o != null ? (string)o : "    ";
-            }
-        set => _options[nameof(IndentString)] = value;
-    }
-
-    public bool UseFullTypeName
-    {
-        get
-        {
-                var o = _options[nameof(UseFullTypeName)];
-                return o != null && (bool)o;
-            }
-        set => _options[nameof(UseFullTypeName)] = value;
-    }
+    public bool UseFullTypeName { get; set; }
 }

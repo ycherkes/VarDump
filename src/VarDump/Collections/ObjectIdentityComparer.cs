ea0d67fd7d281e339bf9cdfd9b7f314cc62d9803
Yevhen Cherkes <ycherkes@outlook.com>
2024-03-12 07:50:05 +0100

    Improve object identity comparison

---
+++ src/VarDump/Collections/ObjectIdentityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace VarDump.Collections;
+
+/// <summary>
+/// Source: https://stackoverflow.com/questions/11240036/what-does-runtimehelpers-gethashcode-do
+/// </summary>
+internal class ObjectIdentityComparer : IEqualityComparer<object>
+{
+    bool IEqualityComparer<object>.Equals(object x, object y)
+    {
+        return ReferenceEquals(x, y);
+    }
+
+    int IEqualityComparer<object>.GetHashCode(object x)
+    {
+        return RuntimeHelpers.GetHashCode(x);
+    }
+}
--- src/VarDump/Visitor/VisitContext.cs
+++ src/VarDump/Visitor/VisitContext.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using VarDump.Collections;
 
 namespace VarDump.Visitor;
 
 public sealed class VisitContext(int maxDepth)
 {
+    private static readonly IEqualityComparer<object> IdentityComparer = new ObjectIdentityComparer();
     private readonly Stack<object> _visitedObjects = new();
 
     public int CurrentDepth { get; set; }
@@ -20,7 +23,7 @@ public sealed class VisitContext(int maxDepth)
 
     public bool IsVisited(object value)
     {
-        return value != null && _visitedObjects.Contains(value);
+        return value != null && _visitedObjects.Contains(value, IdentityComparer);
     }
 
     public bool IsMaxDepth()

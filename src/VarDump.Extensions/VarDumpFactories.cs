80ddc7c15510295b74ae8c8d49b18e60817b0e74
Yevhen Cherkes <ycherkes@outlook.com>
2024-04-02 07:49:59 +0200

    + add Source Link support - remove namespace from VarDumpExtensions package

---
+++ src/VarDump.Extensions/VarDumpFactories.cs
@@ -0,0 +1,8 @@
+﻿using VarDump;
+using VarDump.Visitor;
+
+public class VarDumpFactories
+{
+    public static readonly Func<DumpOptions, IDumper> CSharp = options => new CSharpDumper(options);
+    public static readonly Func<DumpOptions, IDumper> VisualBasic = options => new VisualBasicDumper(options);
+}

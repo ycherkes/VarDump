7263c8e68161c62dc2559479a5b8409e3c82949a
Yevhen Cherkes <ycherkes@outlook.com>
2024-06-03 17:30:25 +0300

    Regression: fix decimal type dumping. (#45)
    Co-authored-by: yevhen.cherkes <yevhen.cherkes@semine.com>

---
+++ test/VarDump.UnitTests/PrimitiveTypesSpec.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace VarDump.UnitTests
+{
+    public class PrimitiveTypesSpec
+    {
+        [Fact]
+        public void DumpDecimalCSharp()
+        {
+            const decimal value = 0.00000M;
+
+            var dumper = new CSharpDumper();
+
+            var result = dumper.Dump(value);
+
+            Assert.Equal("var decimalValue = 0.00000m;\r\n", result);
+        }
+
+        [Fact]
+        public void DumpDecimalVb()
+        {
+            const decimal value = 0.00000M;
+
+            var dumper = new VisualBasicDumper();
+
+            var result = dumper.Dump(value);
+
+            Assert.Equal("Dim decimalValue = 0.00000D\r\n", result);
+        }
+    }
+}

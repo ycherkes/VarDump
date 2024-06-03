417f98656e5be5ebd6f406fc269b31ac1521d0df
Yevhen Cherkes <ycherkes@outlook.com>
2024-06-03 17:38:54 +0200

    add tests that cover IsPrimitive

---
+++ test/VarDump.UnitTests/Utils/ReflectionUtilsSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using VarDump.Utils;
+using Xunit;
+
+namespace VarDump.UnitTests.Utils;
+
+public class ReflectionUtilsSpec
+{
+    [Theory]
+    [InlineData(typeof(char))]
+    [InlineData(typeof(sbyte))]
+    [InlineData(typeof(ushort))]
+    [InlineData(typeof(uint))]
+    [InlineData(typeof(ulong))]
+    [InlineData(typeof(string))]
+    [InlineData(typeof(byte))]
+    [InlineData(typeof(short))]
+    [InlineData(typeof(int))]
+    [InlineData(typeof(long))]
+    [InlineData(typeof(float))]
+    [InlineData(typeof(double))]
+    [InlineData(typeof(decimal))]
+    [InlineData(typeof(bool))]
+    public void IsPrimitive(Type type)
+    {
+        Assert.True(ReflectionUtils.IsPrimitive(type));
+    }
+}

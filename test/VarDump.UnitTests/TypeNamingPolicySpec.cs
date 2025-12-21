be767f7afbdae3fcae7b7a4c6028008e255c092e
ycherkes <ycherkes@outlook.com>
2025-12-21 12:51:52 +0100

    unit tests for type naming policy

---
+++ test/VarDump.UnitTests/TypeNamingPolicySpec.cs
@@ -0,0 +1,55 @@
+using VarDump.Visitor;
+using Xunit;
+
+namespace VarDump.UnitTests;
+
+public class TypeNamingPolicySpec
+{
+    [Theory]
+    [InlineData(TypeNamingPolicy.ShortName,
+        """
+        var inner = new Inner
+        {
+            Name = "Nested value"
+        };
+
+        """)]
+    [InlineData(TypeNamingPolicy.NestedQualified,
+        """
+        var inner = new TypeNamingPolicySpec.NestedParent.Inner
+        {
+            Name = "Nested value"
+        };
+
+        """)]
+    [InlineData(TypeNamingPolicy.FullName,
+        """
+        var inner = new VarDump.UnitTests.TypeNamingPolicySpec.NestedParent.Inner
+        {
+            Name = "Nested value"
+        };
+
+        """)]
+    public void DumpNestedTypeNamesAccordingToPolicy(TypeNamingPolicy policy, string expected)
+    {
+        var dumper = new CSharpDumper(new DumpOptions
+        {
+            TypeNamePolicy = policy
+        });
+
+        var result = dumper.Dump(new NestedParent.Inner
+        {
+            Name = "Nested value"
+        });
+
+        Assert.Equal(expected.ReplaceLineEndings(), result);
+    }
+
+    private class NestedParent
+    {
+        public class Inner
+        {
+            public string Name { get; set; }
+        }
+    }
+}

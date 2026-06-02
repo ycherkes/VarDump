189ae03a717ec860689a44d964f832148b779c3b
Yevhen Cherkes <ycherkes@outlook.com>
2026-06-02 07:43:11 +0200

    * Fix the collection expression indentation * v++

--- src/VarDump/CodeDom/CSharp/CSharpCodeWriter.cs
+++ src/VarDump/CodeDom/CSharp/CSharpCodeWriter.cs
@@ -338,7 +338,6 @@ internal sealed class CSharpCodeWriter : ICodeWriter
         else
         {
             _output.WriteLine();
-            _output.OutputIndents(Indent);
             _output.WriteLine("[");
             OutputActions(initializers, newlineBetweenItems: true);
             _output.WriteLine();
